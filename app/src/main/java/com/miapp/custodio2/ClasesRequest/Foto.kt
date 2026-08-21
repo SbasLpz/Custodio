5d55b3c79f003e330d5b92dedabeb96ddb4331ac
SbasLpz <sebastian.lopezluna5@gmail.com>
2026-08-21 10:46:31 -0600

    Directriz Funcionando - api vieja

--- app/src/main/java/com/miapp/custodio2/ClasesRequest/Foto.kt
+++ app/src/main/java/com/miapp/custodio2/ClasesRequest/Foto.kt
@@ -2,3 +2,5 @@ package com.miapp.custodio2.ClasesRequest
 
 data class Foto(val Id: Int, val Accion:String, val Fecha: String, val Foto: String,
                 val Latitud:String, val Longitud: String, val Token: String)
+//data class Foto(val Token: String, val Accion:String, val Latitud:String, val Longitud: String,
+//                val Foto: String, val TipoFotografia: Int)
